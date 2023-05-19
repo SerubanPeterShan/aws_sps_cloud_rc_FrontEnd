f04ded4dc451f0307d53df9947fc773aee0e2304
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-19 16:58:22 +0530

    cheking

--- Front_end_Automation/main.tf
+++ Front_end_Automation/main.tf
@@ -5,23 +5,17 @@ resource "aws_s3_bucket" "S3bucket" {
     }
 }
 resource "aws_s3_account_public_access_block" "S3bucketpublic" {
-   bucket= aws_s3_bucket.S3bucket.id
+   bucket= "aws-rc-cloud"
   block_public_acls = false
   block_public_policy = false
   ignore_public_acls = false
   restrict_public_buckets = false
 }
 resource "aws_s3_bucket_website_configuration" "s3StaticWeb" {
-  bucket= aws_s3_bucket.S3bucket.id
+  bucket= "aws-rc-cloud"
 
   index_document {
     suffix = "index.html"
   }
 }
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  # ...
 
-  provisioner "local-exec" {
-    command = "aws cloudfront create-invalidation --distribution-id ${self.id} --paths '/*'"
-  }
-}
